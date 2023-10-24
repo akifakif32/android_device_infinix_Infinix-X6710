fea0f1da60ac991272be5c84c1bcd8c70c444422
akifakif32 <120502131+akifakif32@users.noreply.github.com>
2023-10-24 17:23:40 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X6710
+DEVICE_PATH := device/infinix/X6710
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
