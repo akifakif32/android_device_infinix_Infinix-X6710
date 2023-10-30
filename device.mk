2a45a115ee13086d74b10a939442e3238ae3e06f
akifakif32 <120502131+akifakif32@users.noreply.github.com>
2023-10-30 18:14:55 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -27,10 +27,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \ 
-     android.hardware.boot@1.2-impl \
-     android.hardware.boot@1.2-mtkimpl.recovery \
-     android.hardware.boot@1.2-mtkimpl
-
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-mtkimpl
+     
 PRODUCT_PACKAGES += \
     bootctrl.mt6893 \
     bootctrl.recovery \
