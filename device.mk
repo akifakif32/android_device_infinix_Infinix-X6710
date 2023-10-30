9f06022c493a2f1e9849d22e85faef7374362a68
akifakif32 <120502131+akifakif32@users.noreply.github.com>
2023-10-30 13:24:26 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -26,9 +26,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+PRODUCT_PACKAGES += \ 
+     android.hardware.boot@1.2-impl \
+     android.hardware.boot@1.2-mtkimpl.recovery / 
+     android.hardware.boot@1.2-mtkimpl
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6893\
